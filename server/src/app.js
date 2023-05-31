025d1dcec599c8079bbb2d90c4702b7aa451a00b
= <=>
2023-05-31 09:24:19 +0530

    Added models, controllers and routers for planets and launches.

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan")
+const planetsRouter = require("./routes/planets/planets.router");
+const launchesRouters = require("./routes/launches/launches.router");
 
 
 const app = express();
 
+app.use(cors({
+  origin: "http://localhost:3000"
+}));
+app.use(morgan("combined"));
 app.use(express.json());
+
+app.use("/planets", planetsRouter);
+app.use("/launches", launchesRouters);
 
 module.exports = app;
--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,16 @@
 const http = require("http");
 const app = require("./app");
+const { loadPlanetsData } = require("./models/planets.model")
 
 
 const PORT = process.env.PORT || 3001;
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
+async function startUp() {
+  await loadPlanetsData();
+  server.listen(PORT, () => {
     console.log(PORT);
-});
+  });
+}
+
+startUp();
